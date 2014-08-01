955eab4e53eec856b335262938a51d95cadc6610
Shawn <hanlong522@gmail.com>
2014-08-01 09:16:13 +0800

    Initial Commit

---
+++ sqliteObjectDemo02/SQLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SQLAppDelegate.h
+//  sqliteObjectDemo02
+//
+//  Created by Shawn on 14-8-1.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sqliteObjectDemo02/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sqliteObjectDemo02
+//
+//  Created by Shawn on 14-8-1.
+//  Copyright (c) 2014年 hanlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SQLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SQLAppDelegate class]));
+    }
+}
